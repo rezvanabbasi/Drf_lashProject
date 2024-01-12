6a67df6c35b8c3c94acfa30ba8969ef9c0612fc6
RezvanAbbasi <rezvan.abasi555@gmail.com>
2024-01-12 11:33:49 +0330

    complete users test

--- apps/tests/users/test_apis.py
+++ apps/tests/users/test_apis.py
@@ -7,7 +7,7 @@ class UserTypeTest(SetupTest):
             "title": "مربی",
             "description": "this is test"
         }
-        return super(UserTypeTest, self).setUp()
+        return super().setUp()
 
     def test_user_type_create(self):
         response = self.client.post(self.type_url, self.type_data)
@@ -17,7 +17,6 @@ class UserTypeTest(SetupTest):
 class RegisterTest(SetupTest):
     def setUp(self):
         self.register_data = {
-            "id":2,
             "email": "mytest@newtest.com",
             "phone_number": "09188765432",
             "username": "mytestnew",
@@ -27,8 +26,73 @@ class RegisterTest(SetupTest):
             "birth_date": "1997-04-11",
             "user_type": "1",
         }
-        return super(RegisterTest, self).setUp()
+
+        return super().setUp()
 
     def test_register_valid(self):
         response = self.client.post(self.register_url, self.register_data)
         self.assertEquals(response.status_code, 201)
+
+
+class Login(SetupTest):
+    def setUp(self):
+        self.data = {
+            "username": "test",
+            "password": "123456"
+        }
+        self.profile_not_exist_data = {
+            "username": "not found username",
+            "password": "not found password"
+        }
+        return super().setUp()
+
+    def test_login_(self):
+        response = self.client.post(self.login_url, self.data)
+        self.assertEquals(response.status_code, 200)
+
+    def test_profile_not_exist(self):
+        response = self.client.post(self.login_url, self.profile_not_exist_data)
+        self.assertEquals(response.status_code, 400)
+
+
+class ForgetPassword(SetupTest):
+    def setUp(self):
+        self.data = {
+            "mobile": "12345678910"
+        }
+        return super().setUp()
+
+    def test_forget_password(self):
+        response = self.client.post(self.forget_password_url, self.data)
+        self.assertEquals(response.status_code, 200)
+
+
+class ConfirmPassword(SetupTest):
+    def setUp(self):
+        self.equal_data = {
+            "code": "1234",
+            "password": "654321",
+            "confirm_password": "6543d21"
+        }
+        self.unequal_data = {
+            "code": "1234",
+            "password": "123456",
+            "confirm_password": "654321"
+        }
+        self.incomplete_data = {
+            "password": "123456",
+            "confirm_password": "654321"
+        }
+        return super().setUp()
+
+    def test_confirm_equal_password(self):
+        response = self.client.post(self.confirm_password_url, self.equal_data)
+        self.assertEquals(response.status_code, 200)
+
+    def test_confirm_unequal_password(self):
+        response = self.client.post(self.confirm_password_url, self.unequal_data)
+        self.assertEquals(response.status_code, 400)
+
+    def test_not_send_otp_code(self):
+        response = self.client.post(self.confirm_password_url, self.incomplete_data)
+        self.assertEquals(response.status_code, 400)
--- apps/tests/users/test_models.py
+++ apps/tests/users/test_models.py
@@ -1,12 +1,14 @@
-from .test_setup import SetupTest
+from apps.tests.users.test_setup import SetupTest
 
 
 class ModelTest(SetupTest):
+
+    def test_user_type(self):
+        self.assertEquals(str(self.user_type), self.user_type.title)
+
     def test_profile_model(self):
         self.assertEquals(str(self.profile), self.profile.email)
 
-    def test_user_type_model(self):
-        self.assertEquals(str(self.user_type), self.user_type.title)
-
     def test_otp_code(self):
         self.assertEquals(str(self.otp_code), self.otp_code.mobile)
+#
